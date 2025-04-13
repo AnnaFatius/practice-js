a7f08b6baa3bd60b6c8dff34b8b3cbfd2e0cee71
AnnaFatius <anna.fatyus01@gmail.com>
2025-04-13 23:43:50 +0300

    second part of extra task

--- lesson6/extra-task1.js
+++ lesson6/extra-task1.js
@@ -1,3 +1,4 @@
+//Task 1
 function shortLongShort(str1 = '', str2 = '') {
     if(str1.length > str2.length) {
         console.log(`${str2}${str1}${str2}`)
@@ -11,3 +12,37 @@ shortLongShort('AA', 'bbb')
 shortLongShort('1', '22')
 shortLongShort('22', '1')
 shortLongShort('  ', '1')
+
+
+//Task 2
+function checkPairNumber(n) {
+    if(Number.isInteger(n)) {
+        return n % 2 === 0
+    } else {
+        return false
+    }
+}
+
+console.log(checkPairNumber(2))
+console.log(checkPairNumber(3))
+console.log(checkPairNumber(-2))
+console.log(checkPairNumber(-5))
+console.log(checkPairNumber(2.0))
+
+
+//Task 3
+function rentCost(d) {
+    if(d >= 7) {
+        return rentSum = 40 * d - 50
+    } else if (d >= 3 && d < 7) {
+        return rentSum = 40 * d - 20
+    } else {
+        return rentSum = 40 * d
+    }
+}
+
+console.log(rentCost(2))
+console.log(rentCost(3))
+console.log(rentCost(5))
+console.log(rentCost(7))
+console.log(rentCost(8))
